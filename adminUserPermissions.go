9531470ddc9b585d37d32e1240078e0538c93d55
Arran Ubels <arran4@gmail.com>
2023-08-02 15:45:04 +1000

    Initial

---
+++ adminUserPermissions.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"database/sql"
+	_ "embed"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql" // Import the MySQL driver.
+)
+
+var (
+	//go:embed "adminUserPermissions.html.tmpl"
+	adminUserPermissionsTemplate string
+)
+
+// Define your indexitem struct.
+type indexitem struct {
+	Name string // Name of URL displayed in <a href>
+	Link string // URL for link.
+}
+
+// Your index items.
+var indexItems = []indexitem{
+	{Name: "News", Link: "index"},
+	{Name: "FAQ", Link: "faq"},
+	{Name: "Blogs", Link: "blogs"},
+	{Name: "Forum", Link: "forum"},
+	{Name: "Linker", Link: "linker"},
+	{Name: "Bookmarks", Link: "bookmarks"},
+	{Name: "ImageBBS", Link: "imagebbs"},
+	{Name: "Search", Link: "search"},
+	{Name: "Writings", Link: "writings"},
+	{Name: "Information", Link: "information"},
+	{Name: "Preferences", Link: "user"},
+}
+
+// AdminUserPermissionsData holds the data needed for rendering the template.
+type AdminUserPermissionsData struct {
+	Items  []indexitem
+	Level  int
+	UserID int // Replace this with the actual user ID from the session.
+	Rows   []PermissionRow
+}
+
+// PermissionRow holds the data for each permission row.
+type PermissionRow struct {
+	ID     string
+	User   string
+	Email  string
+	Level  string
+	Where  string
+	Delete template.HTML // Store the Delete button as raw HTML.
+}
+
+func adminUserPermissions(w http.ResponseWriter, r *http.Request) {
+	// Get the session.
+	session, err := store.Get(r, sessionName)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Get level from the query parameters or the session.
+	level, err := strconv.Atoi(r.FormValue("level"))
+	if err != nil {
+		level = -1
+	}
+
+	// Get UserID from the session (replace "your-user-id" with the actual key).
+	userID, _ := session.Values["your-user-id"].(int)
+
+	// Prepare the index items.
+	data := AdminUserPermissionsData{
+		Items:  indexItems,
+		Level:  level,
+		UserID: userID,
+	}
+
+	// Query the database (replace "your-db-connection-string" with the actual connection string).
+	db, err := sql.Open("mysql", "your-db-connection-string")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT p.idpermissions, p.level, u.username, u.email, p.section " +
+		"FROM permissions p, users u " +
+		"WHERE u.idusers=p.users_idusers " +
+		"ORDER BY p.level")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var permID, permLevel, username, email, section string
+		err := rows.Scan(&permID, &permLevel, &username, &email, &section)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		data.Rows = append(data.Rows, PermissionRow{
+			ID:    permID,
+			User:  username,
+			Email: email,
+			Level: permLevel,
+			Where: section,
+			Delete: template.HTML(fmt.Sprintf("<form method=\"post\">"+
+				"<input type=\"hidden\" name=\"permid\" value=\"%s\">"+
+				"<input type=\"submit\" name=\"task\" value=\"User Disallow\">"+
+				"</form>", permID)),
+		})
+	}
+
+	// Prepare the HTML template.
+	tmpl := template.Must(template.New("adminUserPermissions").Parse(adminUserPermissionsTemplate))
+
+	// Execute the template with the data and write the output to the response.
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func init() {
+	//http.HandleFunc("/adminUserPermissions", adminUserPermissions)
+}
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang.org/x/oauth2/endpoints"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"golang.org/x/oauth2"
+)
+
+var (
+	// Replace these with your Google OAuth2 credentials
+	clientID     = ""
+	clientSecret = ""
+	redirectURL  = "http://localhost:8080/callback"
+
+	// Change this to your desired session key
+	sessionName = "my-session"
+	sessionKey  = "authenticated"
+	store       = sessions.NewCookieStore([]byte("random-key"))
+
+	oauth2Config = oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     endpoints.Google,
+	}
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/login", loginHandler)
+	r.HandleFunc("/callback", callbackHandler)
+	r.HandleFunc("/logout", logoutHandler)
+
+	http.Handle("/", r)
+
+	log.Println("Server started on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if user is authenticated
+	if !isAuthenticated(r) {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	tmpl := `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Home Page</title>
+		</head>
+		<body>
+			<h1>Welcome, {{ .Email }}</h1>
+			<a href="/logout">Logout</a>
+		</body>
+		</html>
+	`
+
+	t := template.Must(template.New("home").Parse(tmpl))
+	data := map[string]string{"Email": getEmail(r)}
+
+	t.Execute(w, data)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// Generate the URL to redirect the user to Google's consent page
+	url := oauth2Config.AuthCodeURL("", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	if code == "" {
+		http.Error(w, "Failed to get authorization code", http.StatusInternalServerError)
+		return
+	}
+
+	// Exchange the authorization code for an access token
+	token, err := oauth2Config.Exchange(context.Background(), code)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to exchange token: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Store the access token in the session
+	session, _ := store.Get(r, sessionName)
+	session.Values[sessionKey] = token.AccessToken
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	// Clear the session and log the user out
+	session, _ := store.Get(r, sessionName)
+	session.Values[sessionKey] = nil
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func isAuthenticated(r *http.Request) bool {
+	session, _ := store.Get(r, sessionName)
+	accessToken, ok := session.Values[sessionKey]
+	if !ok {
+		return false
+	}
+
+	return accessToken != nil
+}
+
+func getEmail(r *http.Request) string {
+	// Fetch user's email using the access token from the session
+	session, _ := store.Get(r, sessionName)
+	_, ok := session.Values[sessionKey]
+	if !ok {
+		return ""
+	}
+
+	// Here, you can use the access token to fetch the user's email from the Google API
+	// For simplicity, we just return a dummy email
+	return "example@example.com"
+}

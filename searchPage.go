a076c8b134652ad0864fea861716db79fa1fc81d
Arran Ubels <arran4@gmail.com>
2023-08-27 23:47:58 +1000

    Search page requires nothing on it's own.

--- searchPage.go
+++ searchPage.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gorilla/sessions"
 	"log"
 	"net/http"
 )
@@ -15,11 +14,6 @@ func searchPage(w http.ResponseWriter, r *http.Request) {
 		CoreData: r.Context().Value(ContextValues("coreData")).(*CoreData),
 	}
 
-	vars := mux.Vars(r)
-
-	session := r.Context().Value(ContextValues("session")).(*sessions.Session)
-
-	queries := r.Context().Value(ContextValues("queries")).(*Queries)
 	if err := getCompiledTemplates().ExecuteTemplate(w, "searchPage.tmpl", data); err != nil {
 		log.Printf("Template Error: %s", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)

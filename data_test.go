99d6a7001ac9eff7ae89e831ec9d1856a6eb32b2
Arran Ubels <arran4@gmail.com>
2025-06-09 17:46:36 +1000

    Expand project documentation

--- data_test.go
+++ data_test.go
@@ -7,5 +7,7 @@ import (
 )
 
 func TestCompileGoHTML(t *testing.T) {
-	template.Must(template.New("").Funcs(NewFuncs(r)).ParseFS(os.DirFS("./templates"), "*.gohtml"))
+    // The templates rely on a func map created by NewFuncs. For this
+    // compile-time check we do not need an actual request, so pass nil.
+    template.Must(template.New("").Funcs(NewFuncs(nil)).ParseFS(os.DirFS("./templates"), "*.gohtml"))
 }

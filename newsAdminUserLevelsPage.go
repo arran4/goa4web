5632930add49cad2ea47e68945a97fe89588108a
Arran Ubels <arran4@gmail.com>
2025-06-20 23:30:29 +1000

    Implement news admin user levels

--- newsAdminUserLevelsPage.go
+++ newsAdminUserLevelsPage.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func newsAdminUserLevelsPage(w http.ResponseWriter, r *http.Request) {
 	type Data struct {
 		*CoreData
+		UserLevels []*Permission
 	}
 
 	data := Data{
 		CoreData: r.Context().Value(ContextValues("coreData")).(*CoreData),
 	}
 
-	// SKIP. TODO replace completely
-	// Custom Index???
+	queries := r.Context().Value(ContextValues("queries")).(*Queries)
+	rows, err := queries.GetUsersPermissions(r.Context())
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+		default:
+			log.Printf("getUsersPermissions Error: %s", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	}
+	data.UserLevels = rows
+
+	CustomNewsIndex(data.CoreData, r)
 
 	if err := getCompiledTemplates(NewFuncs(r)).ExecuteTemplate(w, "newsAdminUserLevelsPage.gohtml", data); err != nil {
 		log.Printf("Template Error: %s", err)
@@ -57,16 +72,17 @@ func newsAdminUserLevelsAllowActionPage(w http.ResponseWriter, r *http.Request) 
 }
 
 func newsAdminUserLevelsRemoveActionPage(w http.ResponseWriter, r *http.Request) {
-	// TODO
-	/*
-			char *postid = cont.post.getS("id");
-		if (postid == NULL)
-			postid = "0";
-		a4string query("DELETE FROM permissions WHERE idpermissions=%d",
-				atoi(postid));
-		a4mysqlResult *result = cont.sql.query(query.raw());
-		delete result;
-
-	*/
+	queries := r.Context().Value(ContextValues("queries")).(*Queries)
+	permid, err := strconv.Atoi(r.PostFormValue("permid"))
+	if err != nil {
+		log.Printf("strconv.Atoi(permid) Error: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	if err := queries.PermissionUserDisallow(r.Context(), int32(permid)); err != nil {
+		log.Printf("permissionUserDisallow Error: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 	taskDoneAutoRefreshPage(w, r)
 }

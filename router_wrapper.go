6659cbc625c281aef17cbffd64b72ece8c6fec30
Arran Ubels <arran4@gmail.com>
2025-06-23 11:23:05 +1000

    Wrap core middlewares

--- router_wrapper.go
+++ router_wrapper.go
@@ -11,3 +11,14 @@ type routerWrapper interface {
 type routerWrapperFunc func(http.Handler) http.Handler
 
 func (f routerWrapperFunc) Wrap(h http.Handler) http.Handler { return f(h) }
+
+// newMiddlewareChain returns a routerWrapper that wraps a handler with the provided
+// middleware functions in the order supplied.
+func newMiddlewareChain(mw ...func(http.Handler) http.Handler) routerWrapper {
+	return routerWrapperFunc(func(h http.Handler) http.Handler {
+		for i := len(mw) - 1; i >= 0; i-- {
+			h = mw[i](h)
+		}
+		return h
+	})
+}

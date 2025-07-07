8ea5bef4e260710e2fcb039a82dcf3f033d2b499
Arran Ubels <arran4@gmail.com>
2025-07-07 19:08:52 +1000

    Stream process for a4code2html

--- cmd/a4code2html/a4code2html.go
+++ cmd/a4code2html/a4code2html.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"github.com/arran4/goa4web/a4code2html"
-	"io"
 	"log"
 	"os"
 )
 
 func main() {
-	b, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
-	}
 	converter := a4code2html.NewA4Code2HTML()
-	converter.SetInput(string(b))
-	converter.Process()
-	_, err = os.Stdout.WriteString(converter.Output())
-	if err != nil {
+	if err := converter.ProcessReader(os.Stdin, os.Stdout); err != nil {
 		log.Fatal(err)
 	}
 }

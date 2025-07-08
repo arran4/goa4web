632d94cb91fb46ae1b0b613446812a0a319417b9
arran4 <arran4@users.noreply.github.com>
2025-07-08 12:36:34 +0000

    chore(sqlc): regenerate code

--- internal/db/models.go
+++ internal/db/models.go
@@ -276,8 +276,6 @@ type Password struct {
 	CreatedAt       time.Time
 }
 
-// PendingEmail represents a queued email message stored in full including
-// headers and MIME body. The message is addressed to the referenced user.
 type PendingEmail struct {
 	ID         int32
 	ToUserID   int32

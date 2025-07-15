0992c7e03da4c0d85e382ca651f01b446d85ef55
Arran Ubels <arran4@gmail.com>
2025-07-15 23:56:04 +1000

    Use TemplateHandler for flagged posts and moderator logs

--- handlers/forum/adminForumFlaggedPostsPage.go
+++ handlers/forum/adminForumFlaggedPostsPage.go
@@ -1,23 +1,11 @@
 package forum
 
 import (
-	corecommon "github.com/arran4/goa4web/core/common"
-	common "github.com/arran4/goa4web/handlers/common"
-	"log"
+	hcommon "github.com/arran4/goa4web/handlers/common"
 	"net/http"
-
-	"github.com/arran4/goa4web/core/templates"
 )
 
 // adminForumFlaggedPostsPage displays posts flagged for moderator review.
 func AdminForumFlaggedPostsPage(w http.ResponseWriter, r *http.Request) {
-	type Data struct {
-		*CoreData
-	}
-	data := Data{CoreData: r.Context().Value(common.KeyCoreData).(*CoreData)}
-	if err := templates.RenderTemplate(w, "forumFlaggedPostsPage.gohtml", data, corecommon.NewFuncs(r)); err != nil {
-		log.Printf("Template Error: %s", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+	hcommon.TemplateHandler("forumFlaggedPostsPage.gohtml").ServeHTTP(w, r)
 }

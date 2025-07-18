0e57fdd39ae8acb5248448849536d6dfc9c3017c
Arran Ubels <arran4@gmail.com>
2025-07-18 23:51:34 +1000

    Neither are necessary

--- internal/tasks/shared.go
+++
@@ -1,7 +0,0 @@
-package tasks
-
-// EditReplyTask identifies editing an existing comment.
-var EditReplyTask = TaskString("Edit Reply")
-
-// CancelTask represents cancelling the current operation.
-var CancelTask = TaskString("Cancel")
--- internal/tasks/task_constants.go
+++
@@ -1,7 +0,0 @@
-package tasks
-
-// CancelTask cancels the current operation and returns to the previous page.
-const CancelTask TaskString = "Cancel"
-
-// EditReplyTask updates an existing comment.
-const EditReplyTask TaskString = "Edit Reply"

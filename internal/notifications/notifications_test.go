3e8480bdb5f9f05b22436c02244958a919f7b3c5
Arran Ubels <arran4@gmail.com>
2025-07-19 21:10:19 +1000

    Fix notification tests

--- internal/notifications/notifications_test.go
+++ internal/notifications/notifications_test.go
@@ -80,7 +80,6 @@ func TestNotifierNotifyAdmins(t *testing.T) {
 		WithArgs(sql.NullString{String: "a@test", Valid: true}).
 		WillReturnRows(sqlmock.NewRows([]string{"idusers", "email", "username"}).AddRow(1, "a@test", "a"))
 	mock.ExpectExec("INSERT INTO pending_emails").WithArgs(int32(1), sqlmock.AnyArg()).WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectExec("INSERT INTO notifications").WithArgs(int32(1), sqlmock.AnyArg(), sqlmock.AnyArg()).WillReturnResult(sqlmock.NewResult(1, 1))
 	rec := &dummyProvider{}
 	n := New(q, rec)
 	n.NotifyAdmins(context.Background(), &EmailTemplates{}, EmailData{})

4be12f3764345941ec84fe5982fbb4035931e955
Arran Ubels <arran4@gmail.com>
2025-07-20 20:46:04 +1000

    TODO

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -150,6 +150,7 @@ func RecoverMiddleware(next http.Handler) http.Handler {
 		defer func() {
 			if rec := recover(); rec != nil {
 				log.Printf("panic: %v", rec)
+				// TODO ensure it uses the error page template here and everywhere there is an error like this
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			}
 		}()

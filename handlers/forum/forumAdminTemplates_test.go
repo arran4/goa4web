192487ef1b2876f9250d9cd37caed1102436082a
Arran Ubels <arran4@gmail.com>
2025-07-20 14:58:05 +1000

    Add admin notifications for forum structural tasks

---
+++ handlers/forum/forumAdminTemplates_test.go
@@ -0,0 +1,37 @@
+package forum
+
+import (
+	"testing"
+
+	"github.com/arran4/goa4web/core/templates"
+	notif "github.com/arran4/goa4web/internal/notifications"
+)
+
+func requireAdminEmailTemplates(t *testing.T, et *notif.EmailTemplates) {
+	t.Helper()
+	htmlTmpls := templates.GetCompiledEmailHtmlTemplates(map[string]any{})
+	textTmpls := templates.GetCompiledEmailTextTemplates(map[string]any{})
+	if htmlTmpls.Lookup(et.HTML) == nil {
+		t.Errorf("missing html template %s", et.HTML)
+	}
+	if textTmpls.Lookup(et.Text) == nil {
+		t.Errorf("missing text template %s", et.Text)
+	}
+	if textTmpls.Lookup(et.Subject) == nil {
+		t.Errorf("missing subject template %s", et.Subject)
+	}
+}
+
+func TestForumAdminTemplatesExist(t *testing.T) {
+	admins := []notif.AdminEmailTemplateProvider{
+		createThreadTask,
+		replyTask,
+		categoryChangeTask,
+		categoryCreateTask,
+		deleteCategoryTask,
+		threadDeleteTask,
+	}
+	for _, p := range admins {
+		requireAdminEmailTemplates(t, p.AdminEmailTemplate())
+	}
+}

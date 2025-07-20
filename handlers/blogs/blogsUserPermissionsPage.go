c99b8a164de310f639359c0ca806232336c7398d
Arran Ubels <arran4@gmail.com>
2025-07-20 15:51:43 +1000

    Add user role notifications to tasks

--- handlers/blogs/blogsUserPermissionsPage.go
+++ handlers/blogs/blogsUserPermissionsPage.go
@@ -1,6 +1,7 @@
 package blogs
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
@@ -9,11 +10,13 @@ import (
 	common "github.com/arran4/goa4web/core/common"
 	db "github.com/arran4/goa4web/internal/db"
 
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
 
 	handlers "github.com/arran4/goa4web/handlers"
+	"github.com/arran4/goa4web/internal/eventbus"
 	notif "github.com/arran4/goa4web/internal/notifications"
 	"github.com/arran4/goa4web/internal/tasks"
 )
@@ -25,6 +28,7 @@ var userAllowTask = &UserAllowTask{TaskString: TaskUserAllow}
 
 var _ tasks.Task = (*UserAllowTask)(nil)
 var _ notif.AdminEmailTemplateProvider = (*UserAllowTask)(nil)
+var _ notif.TargetUsersNotificationProvider = (*UserAllowTask)(nil)
 
 func (UserAllowTask) AdminEmailTemplate() *notif.EmailTemplates {
 	return notif.NewEmailTemplates("adminNotificationBlogUserAllowEmail")
@@ -46,6 +50,7 @@ var userDisallowTask = &UserDisallowTask{TaskString: TaskUserDisallow}
 
 var _ tasks.Task = (*UserDisallowTask)(nil)
 var _ notif.AdminEmailTemplateProvider = (*UserDisallowTask)(nil)
+var _ notif.TargetUsersNotificationProvider = (*UserDisallowTask)(nil)
 
 func (UserDisallowTask) AdminEmailTemplate() *notif.EmailTemplates {
 	return notif.NewEmailTemplates("adminNotificationBlogUserDisallowEmail")
@@ -171,6 +176,15 @@ func UsersPermissionsPermissionUserAllowPage(w http.ResponseWriter, r *http.Requ
 		Name:         level,
 	}); err != nil {
 		data.Errors = append(data.Errors, fmt.Errorf("permissionUserAllow: %w", err).Error())
+	} else if cd, ok := r.Context().Value(consts.KeyCoreData).(*common.CoreData); ok {
+		if evt := cd.Event(); evt != nil {
+			if evt.Data == nil {
+				evt.Data = map[string]any{}
+			}
+			evt.Data["targetUserID"] = u.Idusers
+			evt.Data["Username"] = u.Username.String
+			evt.Data["Role"] = level
+		}
 	}
 
 	handlers.TemplateHandler(w, r, "runTaskPage.gohtml", data)
@@ -190,8 +204,24 @@ func UsersPermissionsDisallowPage(w http.ResponseWriter, r *http.Request) {
 	}
 	if permidi, err := strconv.Atoi(permid); err != nil {
 		data.Errors = append(data.Errors, fmt.Errorf("strconv.Atoi: %w", err).Error())
-	} else if err := queries.DeleteUserRole(r.Context(), int32(permidi)); err != nil {
-		data.Errors = append(data.Errors, fmt.Errorf("CreateLanguage: %w", err).Error())
+	} else {
+		id, username, role, err2 := roleInfoByPermID(r.Context(), queries, int32(permidi))
+		if err := queries.DeleteUserRole(r.Context(), int32(permidi)); err != nil {
+			data.Errors = append(data.Errors, fmt.Errorf("CreateLanguage: %w", err).Error())
+		} else if err2 == nil {
+			if cd, ok := r.Context().Value(consts.KeyCoreData).(*common.CoreData); ok {
+				if evt := cd.Event(); evt != nil {
+					if evt.Data == nil {
+						evt.Data = map[string]any{}
+					}
+					evt.Data["targetUserID"] = id
+					evt.Data["Username"] = username
+					evt.Data["Role"] = role
+				}
+			}
+		} else {
+			log.Printf("lookup role: %v", err2)
+		}
 	}
 	handlers.TemplateHandler(w, r, "runTaskPage.gohtml", data)
 }
@@ -258,4 +288,55 @@ func UsersPermissionsBulkDisallowPage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	handlers.TemplateHandler(w, r, "runTaskPage.gohtml", data)
+}
+
+func roleInfoByPermID(ctx context.Context, q *db.Queries, id int32) (int32, string, string, error) {
+	rows, err := q.GetPermissionsWithUsers(ctx, db.GetPermissionsWithUsersParams{Username: sql.NullString{}})
+	if err != nil {
+		return 0, "", "", err
+	}
+	for _, row := range rows {
+		if row.IduserRoles == id {
+			return row.UsersIdusers, row.Username.String, row.Name, nil
+		}
+	}
+	return 0, "", "", sql.ErrNoRows
+}
+
+func (UserAllowTask) TargetUserIDs(evt eventbus.Event) []int32 {
+	if id, ok := evt.Data["targetUserID"].(int32); ok {
+		return []int32{id}
+	}
+	if id, ok := evt.Data["targetUserID"].(int); ok {
+		return []int32{int32(id)}
+	}
+	return nil
+}
+
+func (UserAllowTask) TargetEmailTemplate() *notif.EmailTemplates {
+	return notif.NewEmailTemplates("setUserRoleEmail")
+}
+
+func (UserAllowTask) TargetInternalNotificationTemplate() *string {
+	v := notif.NotificationTemplateFilenameGenerator("set_user_role")
+	return &v
+}
+
+func (UserDisallowTask) TargetUserIDs(evt eventbus.Event) []int32 {
+	if id, ok := evt.Data["targetUserID"].(int32); ok {
+		return []int32{id}
+	}
+	if id, ok := evt.Data["targetUserID"].(int); ok {
+		return []int32{int32(id)}
+	}
+	return nil
+}
+
+func (UserDisallowTask) TargetEmailTemplate() *notif.EmailTemplates {
+	return notif.NewEmailTemplates("deleteUserRoleEmail")
+}
+
+func (UserDisallowTask) TargetInternalNotificationTemplate() *string {
+	v := notif.NotificationTemplateFilenameGenerator("delete_user_role")
+	return &v
 }

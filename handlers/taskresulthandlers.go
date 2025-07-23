3d4d5ce13504109bc21617f5da2c5e04425f9ec7
Arran Ubels <arran4@gmail.com>
2025-07-23 11:40:15 +1000

    Template with data handler for code.

--- handlers/taskresulthandlers.go
+++ handlers/taskresulthandlers.go
@@ -1,3 +1,29 @@
 package handlers
 
+import (
+	"net/http"
+)
+
 type RedirectHandler string
+
+type templateWithDataHandler struct {
+	tmpl string
+	data any
+}
+
+var _ http.Handler = (*templateWithDataHandler)(nil)
+
+func TemplateWithDataHandler(tmpl string, data any) any {
+	return &templateWithDataHandler{tmpl: tmpl, data: data}
+}
+
+// TemplateHandler renders the template and handles any template error.
+// Example usage:
+//
+//	type Data struct{ *CoreData }
+//	TemplateHandler(w, r, "page.gohtml", Data{cd})
+//
+// Template helpers are provided via data.CoreData.Funcs(r).
+func (th *templateWithDataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	TemplateHandler(w, r, th.tmpl, th.data)
+}

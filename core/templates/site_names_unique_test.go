9a8159441931624c8973e482cd29ccc7493cd315
Arran Ubels <arran4@gmail.com>
2025-07-24 15:03:49 +1000

    test: check for duplicate site template names

---
+++ core/templates/site_names_unique_test.go
@@ -0,0 +1,39 @@
+package templates
+
+import (
+	"embed"
+	"html/template"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/arran4/goa4web/core/common"
+)
+
+//go:embed site/*.gohtml site/*/*.gohtml
+var uniqueTemplates embed.FS
+
+func TestSiteTemplateNamesUnique(t *testing.T) {
+	r := httptest.NewRequest("GET", "/", nil)
+	cd := &common.CoreData{}
+	tmpl, err := template.New("").Funcs(cd.Funcs(r)).ParseFS(uniqueTemplates,
+		"site/*.gohtml", "site/*/*.gohtml")
+	if err != nil {
+		t.Fatalf("failed to parse templates: %v", err)
+	}
+
+	seen := map[string]string{}
+	for _, tt := range tmpl.Templates() {
+		name := tt.Name()
+		if name == "" {
+			continue
+		}
+		parseName := ""
+		if tt.Tree != nil {
+			parseName = tt.Tree.ParseName
+		}
+		if prev, ok := seen[name]; ok {
+			t.Fatalf("duplicate template name %q found in %s and %s", name, prev, parseName)
+		}
+		seen[name] = parseName
+	}
+}

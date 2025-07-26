3db8f76c8cce8190758ac8f1068f663fa0896efd
Arran Ubels <arran4@gmail.com>
2025-07-26 16:11:52 +1000

    accept runtimeconfig pointer for env map

--- cmd/goa4web/config_as.go
+++ cmd/goa4web/config_as.go
@@ -35,7 +35,7 @@ func defaultMap() map[string]string {
 }
 
 func (c *configAsCmd) asEnvFile() error {
-	current, err := config.ToEnvMap(c.rootCmd.cfg, c.rootCmd.ConfigFile)
+	current, err := config.ToEnvMap(&c.rootCmd.cfg, c.rootCmd.ConfigFile)
 	if err != nil {
 		return fmt.Errorf("env map: %w", err)
 	}
@@ -74,7 +74,7 @@ func (c *configAsCmd) asEnvFile() error {
 }
 
 func (c *configAsCmd) asEnv() error {
-	current, err := config.ToEnvMap(c.rootCmd.cfg, c.rootCmd.ConfigFile)
+	current, err := config.ToEnvMap(&c.rootCmd.cfg, c.rootCmd.ConfigFile)
 	if err != nil {
 		return fmt.Errorf("env map: %w", err)
 	}
@@ -113,7 +113,7 @@ func (c *configAsCmd) asEnv() error {
 }
 
 func (c *configAsCmd) asJSON() error {
-	m, err := config.ToEnvMap(c.rootCmd.cfg, c.rootCmd.ConfigFile)
+	m, err := config.ToEnvMap(&c.rootCmd.cfg, c.rootCmd.ConfigFile)
 	if err != nil {
 		return fmt.Errorf("env map: %w", err)
 	}
@@ -126,7 +126,7 @@ func (c *configAsCmd) asJSON() error {
 }
 
 func (c *configAsCmd) asCLI() error {
-	current, err := config.ToEnvMap(c.rootCmd.cfg, c.rootCmd.ConfigFile)
+	current, err := config.ToEnvMap(&c.rootCmd.cfg, c.rootCmd.ConfigFile)
 	if err != nil {
 		return fmt.Errorf("env map: %w", err)
 	}
--- config/envmap_runtime.go
+++ config/envmap_runtime.go
@@ -10,8 +10,8 @@ import (
 // ToEnvMap converts cfg into a map keyed by environment variable name.
 // The cfgPath argument sets the CONFIG_FILE entry and is used to
 // resolve SESSION_SECRET_FILE when empty.
-func ToEnvMap(cfg RuntimeConfig, cfgPath string) (map[string]string, error) {
-	m := ValuesMap(cfg)
+func ToEnvMap(cfg *RuntimeConfig, cfgPath string) (map[string]string, error) {
+	m := ValuesMap(*cfg)
 
 	fileVals, err := LoadAppConfigFile(core.OSFS{}, cfgPath)
 	if err != nil {

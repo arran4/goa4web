4fe26db799dcc8a3fa7fab6a795729c80af21882
Arran Ubels <arran4@gmail.com>
2025-07-26 15:55:02 +1000

    Add Task interface checks

--- handlers/forum/remake_topic_stats_task.go
+++ handlers/forum/remake_topic_stats_task.go
@@ -6,3 +6,6 @@ import "github.com/arran4/goa4web/internal/tasks"
 type RemakeTopicStatsTask struct{ tasks.TaskString }
 
 var remakeTopicStatsTask = &RemakeTopicStatsTask{TaskString: TaskRemakeStatisticInformationOnForumtopic}
+
+// ensure RemakeTopicStatsTask conforms to tasks.Task
+var _ tasks.Task = (*RemakeTopicStatsTask)(nil)

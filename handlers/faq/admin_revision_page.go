464167981ab06ee94536b86b697f27f0df6db9dc
Arran Ubels <arran4@gmail.com>
2025-07-28 09:49:23 +1000

    Add FAQ revision history and edit pages

---
+++ handlers/faq/admin_revision_page.go
@@ -0,0 +1,49 @@
+package faq
+
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/arran4/goa4web/core/common"
+	"github.com/arran4/goa4web/core/consts"
+	"github.com/arran4/goa4web/handlers"
+	"github.com/arran4/goa4web/internal/db"
+	"github.com/gorilla/mux"
+)
+
+// AdminRevisionHistoryPage shows all revisions for a FAQ entry.
+func AdminRevisionHistoryPage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	queries := r.Context().Value(consts.KeyCoreData).(*common.CoreData).Queries()
+	faq, err := queries.GetFAQByID(r.Context(), int32(id))
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		default:
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	}
+	revs, _ := queries.GetFAQRevisionsForFAQ(r.Context(), int32(id))
+	type Data struct {
+		*common.CoreData
+		Faq       *db.Faq
+		Revisions []*db.FaqRevision
+	}
+	data := Data{
+		CoreData:  r.Context().Value(consts.KeyCoreData).(*common.CoreData),
+		Faq:       faq,
+		Revisions: revs,
+	}
+	handlers.TemplateHandler(w, r, "adminFaqRevisionPage.gohtml", data)
+}
--- handlers/faq/routes.go
+++ handlers/faq/routes.go
@@ -33,17 +33,19 @@ func RegisterRoutes(r *mux.Router, _ *config.RuntimeConfig, navReg *navpkg.Regis
 func RegisterAdminRoutes(ar *mux.Router) {
 	farq := ar.PathPrefix("/faq").Subrouter()
 	farq.Use(handlers.IndexMiddleware(CustomFAQIndex))
-	farq.HandleFunc("/answer", AdminAnswerPage).Methods("GET", "POST").MatcherFunc(noTask())
+	farq.HandleFunc("/answer", AdminAnswerPage).Methods("GET").MatcherFunc(noTask())
 	farq.HandleFunc("/answer", handlers.TaskHandler(answerTask)).Methods("POST").MatcherFunc(answerTask.Matcher())
 	farq.HandleFunc("/answer", handlers.TaskHandler(removeQuestionTask)).Methods("POST").MatcherFunc(removeQuestionTask.Matcher())
 	farq.HandleFunc("/categories", AdminCategoriesPage).Methods("GET")
 	farq.HandleFunc("/categories", handlers.TaskHandler(renameCategoryTask)).Methods("POST").MatcherFunc(renameCategoryTask.Matcher())
 	farq.HandleFunc("/categories", handlers.TaskHandler(deleteCategoryTask)).Methods("POST").MatcherFunc(deleteCategoryTask.Matcher())
 	farq.HandleFunc("/categories", handlers.TaskHandler(createCategoryTask)).Methods("POST").MatcherFunc(createCategoryTask.Matcher())
-	farq.HandleFunc("/questions", AdminQuestionsPage).Methods("GET", "POST").MatcherFunc(noTask())
+	farq.HandleFunc("/questions", AdminQuestionsPage).Methods("GET").MatcherFunc(noTask())
 	farq.HandleFunc("/questions", handlers.TaskHandler(editQuestionTask)).Methods("POST").MatcherFunc(editQuestionTask.Matcher())
 	farq.HandleFunc("/questions", handlers.TaskHandler(deleteQuestionTask)).Methods("POST").MatcherFunc(deleteQuestionTask.Matcher())
 	farq.HandleFunc("/questions", handlers.TaskHandler(createQuestionTask)).Methods("POST").MatcherFunc(createQuestionTask.Matcher())
+	farq.HandleFunc("/question/{id:[0-9]+}/edit", AdminEditQuestionPage).Methods("GET")
+	farq.HandleFunc("/revisions/{id:[0-9]+}", AdminRevisionHistoryPage).Methods("GET")
 }
 
 // Register registers the faq router module.

c437cbf58678b90fbb060cb4ade0f28c166d5437
Arran Ubels <arran4@gmail.com>
2025-07-29 21:25:18 +1000

    admin: add grant management links

---
+++ handlers/admin/role_grant_delete_task.go
@@ -0,0 +1,33 @@
+package admin
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/arran4/goa4web/core/common"
+	"github.com/arran4/goa4web/core/consts"
+	"github.com/arran4/goa4web/handlers"
+	"github.com/arran4/goa4web/internal/tasks"
+)
+
+// RoleGrantDeleteTask removes a grant from a role.
+type RoleGrantDeleteTask struct{ tasks.TaskString }
+
+var roleGrantDeleteTask = &RoleGrantDeleteTask{TaskString: TaskRoleGrantDelete}
+
+var _ tasks.Task = (*RoleGrantDeleteTask)(nil)
+
+func (RoleGrantDeleteTask) Action(w http.ResponseWriter, r *http.Request) any {
+	queries := r.Context().Value(consts.KeyCoreData).(*common.CoreData).Queries()
+	grantID, err := strconv.Atoi(r.PostFormValue("grantid"))
+	if err != nil {
+		return fmt.Errorf("grant id parse fail %w", handlers.ErrRedirectOnSamePageHandler(err))
+	}
+	if err := queries.DeleteGrant(r.Context(), int32(grantID)); err != nil {
+		log.Printf("DeleteGrant: %v", err)
+		return fmt.Errorf("delete grant %w", handlers.ErrRedirectOnSamePageHandler(err))
+	}
+	return nil
+}

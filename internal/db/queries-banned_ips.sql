a107a5dcd71c06ad90052cb3b9dcca5d364fbfcd
Arran Ubels <arran4@gmail.com>
2025-08-01 22:35:36 +1000

    Remove unused GetBannedIpByAddress query

--- internal/db/queries-banned_ips.sql
+++ internal/db/queries-banned_ips.sql
@@ -8,8 +8,6 @@ UPDATE banned_ips SET reason = ?, expires_at = ? WHERE id = ?;
 -- name: CancelBannedIp :exec
 UPDATE banned_ips SET canceled_at = CURRENT_TIMESTAMP WHERE ip_net = ? AND canceled_at IS NULL;
 
--- name: GetBannedIpByAddress :one
-SELECT * FROM banned_ips WHERE ip_net = ?;
 
 -- name: ListActiveBans :many
 SELECT * FROM banned_ips WHERE canceled_at IS NULL AND (expires_at IS NULL OR expires_at > NOW());

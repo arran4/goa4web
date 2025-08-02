3c1058e67cb79f093c9ace0fc1d6165b163c17e7
Arran Ubels <arran4@gmail.com>
2025-08-02 13:03:30 +1000

    Remove redundant password query

--- internal/db/queries-passwords.sql
+++ internal/db/queries-passwords.sql
@@ -2,9 +2,3 @@
 INSERT INTO passwords (users_idusers, passwd, passwd_algorithm)
 VALUES (?, ?, ?);
 
--- name: GetLatestPasswordByUserID :one
-SELECT id, users_idusers, passwd, passwd_algorithm, created_at
-FROM passwords
-WHERE users_idusers = ?
-ORDER BY created_at DESC
-LIMIT 1;

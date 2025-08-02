6b2bfbfaad3da3b800ac72c3ff7c93ba79ca6fd2
Arran Ubels <arran4@gmail.com>
2025-08-02 13:04:24 +1000

    Check login role in password flows and drop unused reset queries

--- cmd/goa4web/user_password_clear_expired.go
+++ cmd/goa4web/user_password_clear_expired.go
@@ -15,7 +15,6 @@ type userPasswordClearExpiredCmd struct {
 	*userPasswordCmd
 	fs    *flag.FlagSet
 	Hours int
-	List  bool
 	args  []string
 }
 
@@ -23,7 +22,6 @@ func parseUserPasswordClearExpiredCmd(parent *userPasswordCmd, args []string) (*
 	c := &userPasswordClearExpiredCmd{userPasswordCmd: parent, Hours: 24}
 	fs := flag.NewFlagSet("clear-expired", flag.ContinueOnError)
 	fs.IntVar(&c.Hours, "hours", 24, "expiration age in hours")
-	fs.BoolVar(&c.List, "list", false, "list removed reset requests")
 	c.fs = fs
 	if err := fs.Parse(args); err != nil {
 		return nil, err
@@ -40,25 +38,12 @@ func (c *userPasswordClearExpiredCmd) Run() error {
 	ctx := context.Background()
 	queries := dbpkg.New(db)
 	expiry := time.Now().Add(-time.Duration(c.Hours) * time.Hour)
-	var details []*dbpkg.ListPasswordResetsBeforeRow
-	if c.List {
-		var err error
-		details, err = queries.ListPasswordResetsBefore(ctx, expiry)
-		if err != nil {
-			return fmt.Errorf("list resets: %w", err)
-		}
-	}
 	res, err := queries.PurgePasswordResetsBefore(ctx, expiry)
 	if err != nil {
 		return fmt.Errorf("clear expired: %w", err)
 	}
 	if rows, err := res.RowsAffected(); err == nil {
 		c.rootCmd.Infof("deleted %d expired password reset requests", rows)
-	}
-	if c.List {
-		for _, r := range details {
-			c.rootCmd.Infof("removed id=%d code=%s created=%s", r.ID, r.VerificationCode, r.CreatedAt.Format(time.RFC3339))
-		}
 	}
 	return nil
 }

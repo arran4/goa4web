077dc15f63585bdb3bb81ac2df1d326cef828391
Arran Ubels <arran4@gmail.com>
2025-08-05 23:46:56 +1000

    Unnecessary imports removed

--- handlers/images/routes_test.go
+++ handlers/images/routes_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -12,9 +11,7 @@ import (
 	"github.com/arran4/goa4web/config"
 	"github.com/arran4/goa4web/core/common"
 	"github.com/arran4/goa4web/core/consts"
-	imagesign "github.com/arran4/goa4web/internal/images"
 	"github.com/arran4/goa4web/internal/navigation"
-	"github.com/arran4/goa4web/internal/sign"
 )
 
 func TestValidID(t *testing.T) {

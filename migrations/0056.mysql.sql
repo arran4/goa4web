719a5231e8f0512d3d68cd75327395844a259007
Arran Ubels <arran4@gmail.com>
2025-08-07 15:42:54 +1000

    Fixed

--- migrations/0056.mysql.sql
+++ migrations/0056.mysql.sql
@@ -1,10 +1,51 @@
 ALTER TABLE preferences ADD COLUMN timezone TINYTEXT DEFAULT NULL;
-UPDATE blogs SET written = CONVERT_TZ(written, 'Australia/Melbourne', 'UTC');
-UPDATE comments SET written = CONVERT_TZ(written, 'Australia/Melbourne', 'UTC');
-UPDATE forumthread SET lastaddition = CONVERT_TZ(lastaddition, 'Australia/Melbourne', 'UTC');
-UPDATE forumtopic SET lastaddition = CONVERT_TZ(lastaddition, 'Australia/Melbourne', 'UTC');
-UPDATE imagepost SET posted = CONVERT_TZ(posted, 'Australia/Melbourne', 'UTC');
-UPDATE linker SET listed = CONVERT_TZ(listed, 'Australia/Melbourne', 'UTC');
-UPDATE writing SET published = CONVERT_TZ(published, 'Australia/Melbourne', 'UTC');
-UPDATE news SET occurred = CONVERT_TZ(occurred, 'Australia/Melbourne', 'UTC');
+
+UPDATE blogs
+  SET written = COALESCE(
+    CONVERT_TZ(written, 'Australia/Melbourne', 'UTC'),
+    written
+  );
+
+UPDATE comments
+  SET written = COALESCE(
+    CONVERT_TZ(written, 'Australia/Melbourne', 'UTC'),
+    written
+  );
+
+UPDATE forumthread
+  SET lastaddition = COALESCE(
+    CONVERT_TZ(lastaddition, 'Australia/Melbourne', 'UTC'),
+    lastaddition
+  );
+
+UPDATE forumtopic
+  SET lastaddition = COALESCE(
+    CONVERT_TZ(lastaddition, 'Australia/Melbourne', 'UTC'),
+    lastaddition
+  );
+
+UPDATE imagepost
+  SET posted = COALESCE(
+    CONVERT_TZ(posted, 'Australia/Melbourne', 'UTC'),
+    posted
+  );
+
+UPDATE linker
+  SET listed = COALESCE(
+    CONVERT_TZ(listed, 'Australia/Melbourne', 'UTC'),
+    listed
+  );
+
+UPDATE writing
+  SET published = COALESCE(
+    CONVERT_TZ(published, 'Australia/Melbourne', 'UTC'),
+    published
+  );
+
+UPDATE site_news
+  SET occurred = COALESCE(
+    CONVERT_TZ(occurred, 'Australia/Melbourne', 'UTC'),
+    occurred
+  );
+
 UPDATE schema_version SET version = 56 WHERE version = 55;

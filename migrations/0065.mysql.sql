a1eac3ab8725207bbff2e477c4cd67e18be2243b
Arran Ubels <arran4@gmail.com>
2025-08-11 16:28:07 +1000

    Fix forumtopic label migrations

--- migrations/0065.mysql.sql
+++ migrations/0065.mysql.sql
@@ -1,17 +1,17 @@
-RENAME TABLE forumthread_public_labels TO content_public_labels;
+RENAME TABLE forumtopic_public_labels TO content_public_labels;
 ALTER TABLE content_public_labels
-    CHANGE COLUMN forumthread_idforumthread item_id INT NOT NULL,
+    CHANGE COLUMN forumtopic_idforumtopic item_id INT NOT NULL,
     ADD COLUMN item VARCHAR(64) NOT NULL DEFAULT 'thread' AFTER id,
-    DROP INDEX forumthread_public_labels_uq,
+    DROP INDEX forumtopic_public_labels_uq,
     ADD UNIQUE INDEX content_public_labels_uq (item, item_id, label(255));
 ALTER TABLE content_public_labels MODIFY item VARCHAR(64) NOT NULL;
 
-RENAME TABLE forumthread_private_labels TO content_private_labels;
+RENAME TABLE forumtopic_private_labels TO content_private_labels;
 ALTER TABLE content_private_labels
-    CHANGE COLUMN forumthread_idforumthread item_id INT NOT NULL,
+    CHANGE COLUMN forumtopic_idforumtopic item_id INT NOT NULL,
     CHANGE COLUMN users_idusers user_id INT NOT NULL,
     ADD COLUMN item VARCHAR(64) NOT NULL DEFAULT 'thread' AFTER id,
-    DROP INDEX forumthread_private_labels_uq,
+    DROP INDEX forumtopic_private_labels_uq,
     ADD UNIQUE INDEX content_private_labels_uq (item, item_id, user_id, label(255));
 ALTER TABLE content_private_labels MODIFY item VARCHAR(64) NOT NULL;
 

96368dbb496df180f4e28f13158acd4b821d1ffc
Arran Ubels <arran4@gmail.com>
2025-08-11 21:00:37 +1000

    feat(news): add edit page for news posts

--- handlers/news/newsEditPostTask.go
+++ handlers/news/newsEditPostTask.go
@@ -1,6 +1,7 @@
 package news
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -13,6 +14,7 @@ import (
 
 	"github.com/arran4/goa4web/core/common"
 	"github.com/arran4/goa4web/handlers"
+	"github.com/arran4/goa4web/internal/db"
 	notif "github.com/arran4/goa4web/internal/notifications"
 	"github.com/arran4/goa4web/internal/tasks"
 )
@@ -32,6 +34,8 @@ func (EditTask) AdminInternalNotificationTemplate(evt eventbus.TaskEvent) *strin
 	v := notif.NotificationTemplateFilenameGenerator("adminNotificationNewsEditEmail")
 	return &v
 }
+
+func (EditTask) Page(w http.ResponseWriter, r *http.Request) { newsEditFormPage(w, r) }
 
 func (EditTask) Action(w http.ResponseWriter, r *http.Request) any {
 	if err := handlers.ValidateForm(r, []string{"language", "text"}, []string{"language", "text"}); err != nil {
@@ -55,4 +59,45 @@ func (EditTask) Action(w http.ResponseWriter, r *http.Request) any {
 	}
 
 	return nil
+}
+
+func newsEditFormPage(w http.ResponseWriter, r *http.Request) {
+	cd := r.Context().Value(consts.KeyCoreData).(*common.CoreData)
+	queries := cd.Queries()
+	pid, err := strconv.Atoi(mux.Vars(r)["news"])
+	if err != nil {
+		http.Redirect(w, r, "/news", http.StatusTemporaryRedirect)
+		return
+	}
+	post, err := queries.GetNewsPostByIdWithWriterIdAndThreadCommentCount(r.Context(), db.GetNewsPostByIdWithWriterIdAndThreadCommentCountParams{
+		ViewerID: cd.UserID,
+		ID:       int32(pid),
+		UserID:   sql.NullInt32{Int32: cd.UserID, Valid: cd.UserID != 0},
+	})
+	if err != nil {
+		http.Redirect(w, r, "/news?error="+err.Error(), http.StatusTemporaryRedirect)
+		return
+	}
+	if !cd.HasGrant("news", "post", "edit", post.Idsitenews) {
+		handlers.RenderErrorPage(w, r, handlers.ErrForbidden)
+		return
+	}
+	langs, err := cd.Languages()
+	if err != nil {
+		handlers.RenderErrorPage(w, r, err)
+		return
+	}
+	cd.PageTitle = "Edit News"
+	data := struct {
+		Languages          []*db.Language
+		Post               *db.GetNewsPostByIdWithWriterIdAndThreadCommentCountRow
+		SelectedLanguageId int
+	}{
+		Languages:          langs,
+		Post:               post,
+		SelectedLanguageId: int(post.LanguageID.Int32),
+	}
+	if err := cd.ExecuteSiteTemplate(w, r, "newsEditPage.gohtml", data); err != nil {
+		handlers.RenderErrorPage(w, r, err)
+	}
 }

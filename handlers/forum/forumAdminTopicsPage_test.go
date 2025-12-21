30a073b86c8fe4bd659f67905f4a9f774d4ea4ca
Arran Ubels <arran4@gmail.com>
2025-12-21 14:43:45 +1100

    Fix admin topics page test setup

--- handlers/forum/forumAdminTopicsPage_test.go
+++ handlers/forum/forumAdminTopicsPage_test.go
@@ -2,6 +2,7 @@ package forum
 
 import (
 	"context"
+	"database/sql"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
@@ -9,8 +10,10 @@ import (
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/gorilla/sessions"
 
 	"github.com/arran4/goa4web/config"
+	"github.com/arran4/goa4web/core"
 	"github.com/arran4/goa4web/core/common"
 	"github.com/arran4/goa4web/core/consts"
 	"github.com/arran4/goa4web/internal/db"
@@ -35,9 +38,25 @@ func TestAdminTopicsPage(t *testing.T) {
 		AddRow(1, 0, 0, "cat", "desc")
 	mock.ExpectQuery(regexp.QuoteMeta("SELECT f.idforumcategory, f.forumcategory_idforumcategory, f.language_id, f.title, f.description FROM forumcategory f")).WithArgs(int32(0), int32(0)).WillReturnRows(categoryRows)
 
+	grantsRows := sqlmock.NewRows([]string{"section", "role_id", "role_name", "user_id", "username"}).
+		AddRow("forum", sql.NullInt32{}, sql.NullString{}, sql.NullInt32{}, sql.NullString{})
+	mock.ExpectQuery("SELECT g.section").WithArgs(sqlmock.AnyArg()).WillReturnRows(grantsRows)
+
+	origStore := core.Store
+	origName := core.SessionName
+	core.Store = sessions.NewCookieStore([]byte("test"))
+	core.SessionName = "test-session"
+	defer func() {
+		core.Store = origStore
+		core.SessionName = origName
+	}()
+
 	cd := common.NewCoreData(context.Background(), db.New(conn), config.NewRuntimeConfig())
 	r := httptest.NewRequest("GET", "/admin/forum/topics", nil)
-	r = r.WithContext(context.WithValue(r.Context(), consts.KeyCoreData, cd))
+	sess, _ := core.Store.New(r, core.SessionName)
+	ctx := context.WithValue(r.Context(), core.ContextValues("session"), sess)
+	ctx = context.WithValue(ctx, consts.KeyCoreData, cd)
+	r = r.WithContext(ctx)
 	w := httptest.NewRecorder()
 
 	AdminTopicsPage(w, r)

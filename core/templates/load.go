da0b708fa21591a3934aa6a503ce84b6a5317202
Arran Ubels <arran4@gmail.com>
2025-12-28 13:52:34 +1100

    Replace LoadSiteTemplates with implementation matching GetCompiledSiteTemplates (preserve assetHash and template naming)

--- core/templates/load.go
+++ core/templates/load.go
@@ -1,7 +1,7 @@
 package templates
 
 import (
-	"html/template"
+	htemplate "html/template"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -10,16 +10,22 @@ import (
 // LoadSiteTemplates parses all .gohtml templates under root using the provided
 // FuncMap. It uses the local filesystem (os.DirFS) and is a convenience wrapper
 // around LoadSiteTemplatesFS.
-func LoadSiteTemplates(funcs template.FuncMap, root string) (*template.Template, error) {
+func LoadSiteTemplates(funcs htemplate.FuncMap, root string) (*htemplate.Template, error) {
 	return LoadSiteTemplatesFS(funcs, os.DirFS(root), ".")
 }
 
 // LoadSiteTemplatesFS parses all .gohtml templates from the provided fs.FS.
 // This allows tests to provide an in-memory fs.FS if they need to avoid
 // touching the real filesystem.
-func LoadSiteTemplatesFS(funcs template.FuncMap, fsys fs.FS, root string) (*template.Template, error) {
-	t := template.New("root").Funcs(funcs)
-	var files []string
+func LoadSiteTemplatesFS(funcs htemplate.FuncMap, fsys fs.FS, root string) (*htemplate.Template, error) {
+	if funcs == nil {
+		funcs = htemplate.FuncMap{}
+	}
+	// ensure assetHash helper is available just like GetCompiledSiteTemplates
+	funcs["assetHash"] = GetAssetHash
+
+	t := htemplate.New("root").Funcs(funcs)
+
 	err := fs.WalkDir(fsys, root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -27,16 +33,21 @@ func LoadSiteTemplatesFS(funcs template.FuncMap, fsys fs.FS, root string) (*temp
 		if d.IsDir() {
 			return nil
 		}
-		if filepath.Ext(path) == ".gohtml" {
-			files = append(files, path)
+		if filepath.Ext(path) != ".gohtml" {
+			return nil
 		}
-		return nil
+
+		b, err := fs.ReadFile(fsys, path)
+		if err != nil {
+			return err
+		}
+
+		// name templates by their relative path to match embedded/template-dir behaviour
+		_, err = t.New(path).Parse(string(b))
+		return err
 	})
 	if err != nil {
 		return nil, err
 	}
-	if len(files) == 0 {
-		return t, nil
-	}
-	return t.ParseFS(fsys, files...)
+	return t, nil
 }

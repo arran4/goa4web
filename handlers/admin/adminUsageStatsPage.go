98791bd591a2c0a7f8479313f92e6f936eb882ec
Arran Ubels <arran4@gmail.com>
2026-02-03 23:08:38 +1100

    build: Add db package import to admin usage stats page.

--- handlers/admin/adminUsageStatsPage.go
+++ handlers/admin/adminUsageStatsPage.go
@@ -9,6 +9,7 @@ import (
 	"github.com/arran4/goa4web/internal/stats"
 
 	"github.com/arran4/goa4web/core/common"
+	"github.com/arran4/goa4web/internal/db"
 	"github.com/arran4/goa4web/internal/tasks"
 )
 

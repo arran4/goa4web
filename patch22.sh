b6ae9bf9a079e10040a1a9ceecc8ec5f94b30456
Arran Ubels <arran4@gmail.com>
2026-07-16 16:07:57 +1000

    Removed unnecessary files

--- patch22.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's/if parsed.Host != cd.Config.HTTPHostname/if cd.Config != nil \&\& parsed.Host != cd.Config.HTTPHostname/g' core/common/coredata.go
--- patch23.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's/case \*ast.QuoteOf:/case \*ast.QuoteOf, \*ast.Quote:/g' a4code/quote.go

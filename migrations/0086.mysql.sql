ee23c627619d081d83a05a46db26e393f67ea0d3
Arran Ubels <arran4@gmail.com>
2026-07-22 15:51:46 +1000

    Fix

--- migrations/0086.mysql.sql
+++ migrations/0086.mysql.sql
@@ -1,7 +1,2 @@
--- +goose Up
 ALTER TABLE preferences ADD COLUMN image_safe_dimension VARCHAR(50);
 UPDATE schema_version SET version = 86 WHERE version = 85;
-
--- +goose Down
-ALTER TABLE preferences DROP COLUMN image_safe_dimension;
-UPDATE schema_version SET version = 85 WHERE version = 86;
